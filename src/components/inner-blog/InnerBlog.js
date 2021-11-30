cdbbec15edd127d7c6768c592b11112ad17a0568
bikramoli <bikramoli75@gmail.com>
2021-11-30 08:52:14 +0545

    Inner blog UI changed along with its product map

--- src/components/inner-blog/InnerBlog.js
+++ src/components/inner-blog/InnerBlog.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card } from 'reactstrap';
 
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { Datas } from '../outer-blog/Data';
@@ -14,21 +13,20 @@ function InnerBlog() {
     var { id } = useParams();
 
     return (
-        <div style={{ background:'lightgray', textAlign:'center'}}>
+        <div style={{ background:'lightgray'}}>
             <div className="innerblogCover">
-                <div className="innerBlog">
-                    <h1>hello</h1>
-                    {/* <strong className="tag-1">* {Datas[id].tag} *</strong>
+                <div className="innerBlog" >
+                   
+                    
+                    <strong className="tag-1">* {Datas[id].tag} *</strong>
                     <span><strong className="author">Author: {Datas[id].author}</strong></span>
                     <h2>{Datas[id].title}</h2>
                     <img src={Datas[id].img}></img>
                     <p>{Datas[id].description}</p>
-                    <p>{Datas[1].content}</p> */}
+                    <p>{Datas[1].content}</p>
                     
                 </div>
-                <div style={{ marginBottom: "4rem" }}>
-                    <Comment2 /> 
-                </div>               
+                    <Comment2 />               
             </div>  
         </div>
     )
